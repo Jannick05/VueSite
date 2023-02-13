67ae1dbcf4eeebb5857330a53513d180c4e0324f
Jannick05 <Jannick_05@icloud.com>
2023-02-13 22:51:12 +0100

    Startup

---
+++ src/components/HomePage.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+  <div class="nigga">
+    <div class="lort">
+      hej
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.nigga {
+  .lort {
+    height: 100px;
+    background-color: black;
+  }
+}
+</style>
